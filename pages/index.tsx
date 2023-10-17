8d78f84d29b818bb2f00c012ee3368d3e2eef6df
yoavporat <yp.yoavporat@gmail.com>
2023-10-17 05:13:03 +0300

    fix: simple cta

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,37 +11,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={`${styles.main}`}>
-        <div className={`${styles.scroll}`}>
-          <div className={`${styles.heading}`}>
-            <Image
-              src="/logo.svg"
-              alt="logo"
-              className={styles.logo}
-              width={100}
-              height={24}
-              priority
-            />
-            <h1>כתף לעוטף</h1>
-            <h3>עוזרים לתושבי הדרום לשקם את הבית</h3>
-          </div>
-          <section>
-            <p>
-              אתם תורמים לנו רהיטים, ציוד מטבח ומכשירים חשמליים, במצב מצוין
-              בלבד, ואנחנו מרכזים אותם במחסנים שלנו במושב ומתכוננים ליום שאחרי,
-              שבו נוכל לעזור למי שמשקם את ביתו, ואולי נעניק שביב של התחלה קצת
-              יותר קלה בתוך כל הכאוס 🙏🏽
-            </p>
-            <h3>מי אנחנו?</h3>
-            <p>אנחנו החבר׳ה ממחזור מ׳ בבית ספר עמי אסף, ילידי 86</p>
-          </section>
+        <div className={`${styles.heading}`}>
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            className={styles.logo}
+            width={100}
+            height={24}
+            priority
+          />
+          <h1>כתף לעוטף</h1>
+          <h3>עוזרים לתושבי הדרום לשקם את הבית</h3>
         </div>
-        <footer className={`${styles.cta}`}>
-          <h3>
+        <section>
+          <p>
+            אתם תורמים לנו רהיטים, ציוד מטבח ומכשירים חשמליים, במצב מצוין בלבד,
+            ואנחנו מרכזים אותם במחסנים שלנו במושב ומתכוננים ליום שאחרי, שבו נוכל
+            לעזור למי שמשקם את ביתו, ואולי נעניק שביב של התחלה קצת יותר קלה בתוך
+            כל הכאוס 🙏🏽
+          </p>
+          <h3 className={`${styles.cta}`}>
             <Link href="https://docs.google.com/forms/d/e/1FAIpQLSdyMJst3WVw_V8Yxzb_h3-kByOl_bqH9he4MJ8-K2hrMsUL2w/viewform">
               אני רוצה לתת כתף!
             </Link>
           </h3>
-        </footer>
+          <h3>מי אנחנו?</h3>
+          <p>אנחנו החבר׳ה ממחזור מ׳ בבית ספר עמי אסף, ילידי 86</p>
+        </section>
       </main>
     </>
   );
