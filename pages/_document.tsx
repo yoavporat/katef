5495d9ff9135149c95cb2f20d810dae9d8ff6f5b
yoavporat <yp.yoavporat@gmail.com>
2023-10-19 22:32:06 +0300

    fix: add bold font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Heebo:wght@200&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Heebo:wght@200;700&display=swap"
           rel="stylesheet"
         />
         <meta
