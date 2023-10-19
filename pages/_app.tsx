0c36bc1a7355cbc162e018780724c98a07a8b65a
yoavporat <yp.yoavporat@gmail.com>
2023-10-19 22:50:30 +0300

    fix: og copy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,15 +9,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta
           name="description"
           property="og:description"
-          content="אנחנו אוספים ריהוט, ציוד מטבח ומכשירי חשמל במצב מצויין או חדש בלבד כדי שתושבי הדרום יוכלו לבחור מקטלוג כשירצו ויקבלו את מה שבחרו עד הבית"
-        ></meta>
+          content="אוספים ריהוט ומכשירי חשמל לתושבי הדרום"
+        />
         <meta
           name="title"
           property="og:title"
           content="כתף לעוטף | עוזרים לתושבי הדרום לשקם את הבית"
-        ></meta>
-        <meta property="og:url" content="https://www.kateflaotef.co.il/"></meta>
-        <meta name="type" property="og:type" content="website"></meta>
+        />
+        <meta property="og:url" content="https://www.kateflaotef.co.il/" />
+        <meta name="type" property="og:type" content="website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
